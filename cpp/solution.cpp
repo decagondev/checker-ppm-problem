2a25a7b76c436befae942091d798429ad9f8c940
Tom Tarpey <tomtarpeydev@gmail.com>
2026-01-01 21:34:38 +0000

    Add solution.cpp file to implement a binary PPM checkerboard image generator with specified dimensions and colors

---
+++ cpp/solution.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+
+/**
+ * Generate a checkerboard.ppm file with the following specification:
+ * - Binary PPM format (P6)
+ * - Image dimensions: 960 × 540 pixels
+ * - Checkerboard made of 60 × 60 pixel squares
+ * - Alternating pure red (255, 0, 0) and pure black (0, 0, 0)
+ * - Top-left square must be red
+ * - No external libraries beyond <fstream>
+ */
+int main() {
+    // Your code here
+
+    return 0;
+}
