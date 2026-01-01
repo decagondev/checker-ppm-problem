e71145a04bfb06e0f5cba8a79c868c4aadc2c057
Tom Tarpey <tomtarpeydev@gmail.com>
2026-01-01 21:34:42 +0000

    Add solution.java file to implement a binary PPM checkerboard image generator with specified dimensions and colors

---
+++ java/Solution.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+
+/**
+ * Generate a file named "checkerboard.ppm" in binary PPM (P6) format.
+ *
+ * Requirements:
+ * - Image dimensions: 960 × 540 pixels
+ * - Checkerboard made of 60 × 60 pixel squares
+ * - Alternating pure red (RGB: 255, 0, 0) and pure black (RGB: 0, 0, 0)
+ * - Top-left square must be red
+ * - Write correct P6 header followed by raw RGB bytes
+ * - Handle IOException appropriately
+ * - No external libraries
+ */
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        // Your code here
+    }
+}
